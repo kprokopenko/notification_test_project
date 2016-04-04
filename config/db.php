a5a0175ea08a08c984598b7539fa26ac3379225d
prokopenko <prokopenko@skytracking.ru>
2016-04-04 11:23:44 +0300

    T-19 Heroku DB config  #time 20m

--- config/db.php
+++ config/db.php
@@ -1,5 +1,19 @@
 <?php
 
+$heroku = getenv("CLEARDB_DATABASE_URL");
+
+if ($heroku) {
+    $url = parse_url($heroku);
+
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=' . $url['host'] . ';dbname=' . substr($url["path"],1),
+        'username' => $url['user'],
+        'password' => $url['pass'],
+        'charset' => 'utf8',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
